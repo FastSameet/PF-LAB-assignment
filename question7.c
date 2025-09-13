916c7776e35d33550f86ed398e4e419458257795
FastSameet <k250857@nu.edu.pk>
2025-09-13 18:33:56 +0500

    Create question7.c

---
+++ question7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int pass;
+    printf("enter your password:");
+    scanf("%d",&pass);
+    if(pass==1234){
+    	printf("Access Granted");}
+	else{
+		printf("Access Denied");
+	}
+	return 0;
+}
